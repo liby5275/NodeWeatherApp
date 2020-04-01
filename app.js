316bc7db48d6326f4b8a07980a283220570d52e3
liby5275 <bibinthomas1991@gmail.com>
2020-04-01 08:33:01 -0700

    commiting changes

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ const weather = require('./weather.js')
 const fs = require('fs')
 
 yargs.command({
-    command:'location',
-    describe:'enter the location to get the weather',
-    builder:{
-        location:{
-            describe:'location',
-            type:'string',
-            demandOption:true
+    command: 'location',
+    describe: 'enter the location to get the weather',
+    builder: {
+        location: {
+            describe: 'location',
+            type: 'string',
+            demandOption: true
         }
     },
-    handler(argv){
-       weather.processLocation(argv.location, (callbackWoeid) => {
-           weather.getWeatherInfo(callbackWoeid, (callbackError,callbackdata) =>{
-            console.log('error occured is '+callbackError)
-            console.log('todays weather of your location is '+callbackdata.consolidated_weather[0].weather_state_name)
-           })
-       })
-       
+    handler(argv) {
+        weather.processLocation(argv.location, (callbackWoeid) => {
+            weather.getWeatherInfo(callbackWoeid, (callbackError, callbackdata) => {
+                console.log('error occured is ' + callbackError)
+                const { weather_state_name: currentWeather } = callbackdata
+                console.log('todays weather of your location is ' +currentWeather)
+            })
+        })
+
     }
 }).argv
 
--- weather.js
+++ weather.js
@@ -2,39 +2,40 @@ const request = require('request')
 
 const processLocation = (location, callback) => {
     const urlTogetWoeid = 'https://www.metaweather.com/api/location/search/?query=' + location
-    
-    request({url:urlTogetWoeid, json:true}, (error, response) => {
-        if(error){
+
+    request({ url: urlTogetWoeid, json: true }, (error, response) => {
+        if (error) {
             console.log('error occured. please check your URL');
-        } else if(response.body.length === 0) {
+        } else if (response.body.length === 0) {
             console.log('location not found! please enter a valid locaton')
-        } 
+        }
         else {
-        const dataJson = response.body;
-        const woeid = dataJson[0].woeid
-        callback(woeid)
+            const dataJson = response.body;
+            const woeid = dataJson[0].woeid
+            callback(woeid)
         }
     })
-    
+
 }
 
 
 
-const getWeatherInfo = (woeid, callback) => {  
-const url = 'https://www.metaweather.com/api/location/'+woeid
+const getWeatherInfo = (woeid, callback) => {
+    console.log(woeid)
+    const url = 'https://www.metaweather.com/api/location/' + woeid
 
-request({url:url, json:true}, (error, response) => {
-    if(error){
-        callback('error occured', undefined)
-    } else{
-    const dataJson = response.body
-    callback(undefined,dataJson)
-} 
-}) 
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('error occured', undefined)
+        } else {
+            const dataJson = response.body
+            callback(undefined, dataJson.consolidated_weather[0])
+        }
+    })
 
 }
 
 module.exports = {
-    processLocation:processLocation,
-    getWeatherInfo:getWeatherInfo
+    processLocation: processLocation,
+    getWeatherInfo: getWeatherInfo
 }
